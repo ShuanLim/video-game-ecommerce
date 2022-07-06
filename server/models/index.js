734d4d774e565fe547dfec0055311a0c58a41ecb
Vicente Garcia <vicen1@hotmail.com>
2022-07-05 21:24:04 -0500

    Add models to index

--- server/models/index.js
+++ server/models/index.js
@@ -8,6 +8,12 @@
 // Declare and import User model
 const User = require('./User');
 // Declare and import Cart model
-const Order = require('./Cart');
-// Export User and Cart models
-module.exports = { User, Cart };
+const Cart = require('./Cart');
+// Declare and import Game model
+const Game = require('./Game');
+// Declare and import Platform model
+const Platform = require('./Platform');
+// Declare and import Genre model
+const Genre = require('./Genre');
+// Export User, Cart, Game, Platform and Genre models
+module.exports = { User, Cart, Game, Platform, Genre };
