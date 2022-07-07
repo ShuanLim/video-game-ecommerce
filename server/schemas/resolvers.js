ea1bc7fcefc871b30d29f438243760afd69e9f1c
Stebbi <stefodd@yahoo.com>
2022-07-07 18:11:24 -0500

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,31 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPW = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        }
     }
 }
