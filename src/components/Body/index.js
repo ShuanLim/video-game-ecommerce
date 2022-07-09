cfed6f186d47b8c9769fe3ebd4bacc7558a83814
Travis Swift <travis.arthur.swift@gmail.com>
2022-07-09 13:56:01 -0500

    final push before merging

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,76 +1,65 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
- 
+
 
 
 function Body() {
-// USE state with array 
+  // USE state with array 
 
-return (
-  <section>
-    {/* <h1>Checkout our super awesome games!</h1>
-
-<div class="ui small images">
-<img src="mariobro.png" alt='Mario Brother'/>
-<img src="bf2042cover.png" alt='BF 2042'/>
-<img src="halo4.png" alt='Halo4'/>
-<img src="godsofwar.png" alt='Gods of War'/>
-</div> */}
+  return (
+    <section>
 
 
-
- 
-
-{/* // USe effect with fetch
+      {/* // USe effect with fetch
 // fetch API 
 // setting state array value to what is rturend from the fetch */}
 
- {/* Use query hooks and probably need at least one mutations with appolo server */}
+      {/* Use query hooks and probably need at least one mutations with appolo server */}
 
 
-  {/* map function */}
- 
-  <div class="ui four column divided relaxed grid">
-  <div class="row">
-    <div class="column ui small images">
-      <p><img src="farcry.png" className='small images' alt='Mario Brother'/> </p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="madden2002.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="sonicforces.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="zelda.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-  </div>
-  <div>
-        <h2 style={{textAlign: "center"}}>Hottest Deals!</h2>
-        <p style={{textAlign: "center"}}>
-       Don't wait buy now
-        </p>
+      {/* map function */}
+
+      <div class="ui four column divided relaxed grid">
+        <div class="row">
+          <div class="column ui small images">
+            <p><img src="farcry.png" className='small images' alt='Mario Brother' /> </p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="madden2002.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="sonicforces.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="zelda.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+        </div>
+        <div>
+          <h2 style={{ textAlign: "center" }}>Hottest Deals!</h2>
+          <p style={{ textAlign: "center" }}>
+            Don't wait buy now
+          </p>
+        </div>
+        <div class="row">
+          <div class="column ui small images">
+            <p><img src="cod.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="xmen.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="reddead.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="ghostbusters.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+        </div>
       </div>
-  <div class="row">
-    <div class="column ui small images">
-      <p><img src="cod.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="xmen.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="reddead.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="ghostbusters.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-  </div>
-</div>
 
     </section>
 
-    
-    
+
+
   );
 }
 
--- src/components/Sale/index.js
+++ src/components/Sale/index.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
- 
+
 
 
 function Sale() {
 
 
-return (
-  <section>
+  return (
+    <section>
 
-  <div class="ui four column divided relaxed grid">
-  <div class="row">
-    <div class="column ui small images">
-      <p><img src="spaceinvaders.png" className='small images' alt='Mario Brother'/> </p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="pacman.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="fallout3.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="tempest.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-  </div>
-  <div>
-        <h2 style={{textAlign: "center"}}>Gotta play 'em all!</h2>
-        <p style={{textAlign: "center"}}>
-       Buy two games and get free shipping
-        </p>
+      <div class="ui four column divided relaxed grid">
+        <div class="row">
+          <div class="column ui small images">
+            <p><img src="spaceinvaders.png" className='small images' alt='Mario Brother' /> </p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="pacman.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="fallout3.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="tempest.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+        </div>
+        <div>
+          <h2 style={{ textAlign: "center" }}>Gotta play 'em all!</h2>
+          <p style={{ textAlign: "center" }}>
+            Buy two games and get free shipping
+          </p>
+        </div>
+        <div class="row">
+          <div class="column ui small images">
+            <p><img src="legostarwars.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="borderlands3.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="pikman.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+          <div class="column ui small images">
+            <p><img src="mariogalaxy.png" className='small images' alt='Mario Brother' /></p>
+          </div>
+        </div>
       </div>
-  <div class="row">
-    <div class="column ui small images">
-      <p><img src="legostarwars.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="borderlands3.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="pikman.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-    <div class="column ui small images">
-      <p><img src="mariogalaxy.png" className='small images' alt='Mario Brother'/></p>
-    </div>
-  </div>
-</div>
 
     </section>
   );
