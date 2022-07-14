c26f8adc1f1f171d015be8ec985df612eed23fb2
Vicente Garcia <vicen1@hotmail.com>
2022-07-13 22:01:00 -0500

    Update connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,7 +8,7 @@
 // Declare and import mongoose to use
 const mongoose = require('mongoose');
 // Establish connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/video-game-ecommerce', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/video-game-ecommerce', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
